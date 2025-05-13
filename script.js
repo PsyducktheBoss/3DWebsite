9b77f3f45b13763727515ca6fac74f62a487a41d
QuayD <164822236+QuayDee@users.noreply.github.com>
2025-05-12 22:53:29 -0400

    Update script.js

--- script.js
+++ script.js
@@ -38,7 +38,7 @@ camera.position.set(0, 0, 5);
 // Vertical drag to move camera up/down
 let isDragging = false;
 let previousMouseY = 0;
-const verticalLimit = { min: -4, max: 4 };
+const verticalLimit = { min: -4, max: 5 };
 
 renderer.domElement.addEventListener('mousedown', (e) => {
   isDragging = true;
